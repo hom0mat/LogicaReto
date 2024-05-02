0e1225a7d85cd001ce78e811e724dc8726b603e2
Mateo Sanchez <168717891+hom0mat@users.noreply.github.com>
2024-05-02 02:44:17 -0600

    Create inst_mem.v

---
+++ Gumnut/inst_mem.v
@@ -0,0 +1,16 @@
+module inst_mem ( input  clk_i,
+         	  input  cyc_i,
+         	  input  stb_i,
+         	  output ack_o,
+         	  input  [11:0] adr_i,
+         	  output [17:0] dat_o );
+
+  reg [17:0] IMem [0:4095];
+
+  initial $readmemh("gasm_text.dat", IMem);
+
+  assign dat_o = IMem[adr_i];
+
+  assign ack_o = cyc_i & stb_i;
+
+endmodule
